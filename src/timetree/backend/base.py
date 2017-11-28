3a839d24c4774132bc2c2cb68c213fd92caebe5a
Andrew He <he.andrew.mail@gmail.com>
2017-11-27 21:08:49 -0500

    Update backend interface to be more object oriented

--- src/timetree/backend/base.py
+++ src/timetree/backend/base.py
@@ -8,13 +8,14 @@ class BaseBackend(metaclass=ABCMeta):
     Timetree's general goal is to express persistent data structures:
     structures in which we can "travel back in time" to a previous state. To
     do this, we'll represent the state of the data structure at each
-    particular "point in time" or "version" as a separate pointer
-    machine of "vnodes" (virtual or versioned nodes). Vnodes are each bound
-    to a particular version, and each have fields keyed by strings, which can
-    be manipulated with :py:meth:`.get`, :py:meth:`.set` and
-    :py:meth:`.delete`. Fields may point to other vnodes in the same machine
-    or to external (ideally immutable) data not in the timetree, but *not* to
-    other versions in the timetree.
+    particular "point in time" or "version" as a separate pointer machine of
+    "vnodes" (virtual or versioned nodes), represented by classes
+    `BaseVersion` and `BaseVnode`. Vnodes are each bound to a particular
+    version, and each have fields keyed by strings, which can be manipulated
+    with :py:meth:`.get`, :py:meth:`.set` and :py:meth:`.delete`. Fields may
+    point to other vnodes in the same machine or to external (ideally
+    immutable) data not in the timetree, but *not* to other versions in the
+    timetree.
 
     The structure of the versions can take on various forms, but most
     generally, they form a DAG. Edges in the DAG represent implicit copies:
@@ -37,19 +38,19 @@ class BaseBackend(metaclass=ABCMeta):
     pointer machine. We provide two different operations to create new versions
     and heads.
 
-    The first is :py:meth:`.branch(commits, vnodes)`, which takes a commit or a
-    list of commits, as well as a list of vnodes bound to those commits.
-    Branch returns a new head based off of all commits given, and returns
-    vnodes bound to the new head corresponding to copies of the input vnodes.
+    The first is :py:meth:`.branch(vnodes)`, which takes a list of vnodes.
+    Branch returns a new head based off of all commits of vnodes given,
+    and returns vnodes bound to the new head corresponding to copies of the
+    input vnodes.
 
-    Second, we have :py:meth:`.commit(head, vnodes)`. This takes a single
-    head version and creates a (frozen) copy of it (a commit). This commit
-    shares the same parents as the head in the version DAG, and we update the
-    head's parent to be only the newly created commit (we split the head into
-    an edge). Alternatively, we mark the head as frozen, create a new head on
-    top, and update all vnodes originally bound to head to bind to the new
-    head. We return the commit, as well as corresponding copies of argument
-    `vnodes`, but rebound to commit.
+    Second, we have :py:meth:`.commit(vnodes)`. This takes a list of vnodes
+    bound to a single head version and creates a (frozen) copy of it (a
+    commit). This commit shares the same parents as the head in the version
+    DAG, and we update the head's parent to be only the newly created commit
+    (we split the head into an edge). Alternatively, we mark the head as
+    frozen, create a new head on top, and update all vnodes originally bound
+    to head to bind to the new head. We return the commit, as well as
+    corresponding copies of argument `vnodes`, but rebound to commit.
 
     To make the operations clearer, we show an alternative view of the version
     pool. We consider the set of only commits. Then, a "head" is a working
@@ -60,72 +61,25 @@ class BaseBackend(metaclass=ABCMeta):
     words.
     """
 
+    __slots__ = ()
+
     @abstractmethod
     def get_base_commit(self):
         """ Get the base commit, on which we can build new versions
 
-        :return: An identifier for the base commit
+        :return: A BaseVersion object corresponding to the commit
         """
 
     @abstractmethod
-    def new_node(self, head):
-        """ Create a new vnode in the specific head version
-
-        :param head: The head version
-        :return: A new vnode in the head version
-        """
-        if not self.is_head(head):
-            raise ValueError("Can only create in head versions")
-
-    @abstractmethod
-    def get(self, vnode, field):
-        """ Get a field of a vnode
-
-        :param vnode: Vnode to access
-        :param field: Field name
-        :return: Field value
-        :raises KeyError: Field not found in vnode
-        """
-
-    @abstractmethod
-    def _is_vnode(self, value):
-        """ Check if a value is a vnode
+    def is_vnode(self, value):
+        """ Check if a value is a vnode of this backend
 
         :param value: The value to check
         :return: True if it is a vnode
         """
 
     @abstractmethod
-    def set(self, vnode, field, value):
-        """ Set a field of a vnode
-
-        :param vnode: Vnode to modify
-        :param field: Field name
-        :param value: New value to set
-            Must be in the same version if it's also a vnode.
-        :return: None
-        :raises ValueError: Value is a vnode but isn't at the same version
-        """
-        if not self.is_head(self.get_version(vnode)):
-            raise ValueError("Can only set in head versions")
-        if self._is_vnode(value):
-            if self.get_version(vnode) != self.get_version(value):
-                raise ValueError("Mismatched versions")
-
-    @abstractmethod
-    def delete(self, vnode, field):
-        """ Delete a field of a vnode
-
-        :param vnode: Vnode to delete from
-        :param field: Field name
-        :return: None
-        :raises KeyError: Field not found in vnode
-        """
-        if not self.is_head(self.get_version(vnode)):
-            raise ValueError("Can only delete from head versions")
-
-    @abstractmethod
-    def commit(self, head, vnodes):
+    def commit(self, vnodes):
         """ Create a new commit based on the given head
 
         In different views, this can be:
@@ -135,58 +89,141 @@ class BaseBackend(metaclass=ABCMeta):
             and implicitly rebind all references to head to the new head.
             - Add a copy of head as a commit to the pool of commits.
 
-        :param head: Head to base the commit on
         :param vnodes: Vnodes which we would like references to; must be
-        bound to `head`
+        bound to the same head
         :return: Reference to the new commit, a list of `vnodes` rebound to it
         """
-        vnodes = list(vnodes)
-        assert all(self.get_version(vnode) == head for vnode in vnodes)
-        return head, vnodes
+        if not all(self.is_vnode(vnode) for vnode in vnodes):
+            raise ValueError('Invalid vnode in commit')
+
+        if vnodes:
+            head = vnodes[0].version
+            if not all(vnode.version == head for vnode in vnodes):
+                raise ValueError('Vnodes must all have the same version')
+            if not head.version.is_head:
+                raise ValueError('Vnode version not a head')
 
     @abstractmethod
-    def branch(self, commit, vnodes, *args):
-        """ Create a new head based on the given commits
+    def branch(self, vnodes):
+        """ Create a new head based on the given vnodes
 
-        Should be called as `branch(commit, vnodes[, commit2, vnodes2, ...])`
         The head's pointer machine is the disjoint union of the pointer
-        machines of the originating commits.
+        machines of the originating commits of the given vnodes.
 
-        :param commit, commit2, ...: Commits to include in the head
-        :param vnodes, vnodes2, ...: Vnodes which we would like references to;
-        must be bound to corresponding commit.
-        :return: Reference to the new head and a list of lists of `vnodes`
-        rebound to it
+        :param vnodes: Vnodes which we would like references to; must all be
+        bound to commits
+        :return: Reference to the new head and a list of `vnodes` rebound to it
         """
-        # The default implementation verifies arguments and returns a list of
-        #  pairs [(commit, vnodes), ...]
-        commits = [commit]
-        commits.extend(args[0::2])
-        vnodess = [vnodes]
-        vnodess.extend(args[1::2])
+        # The default implementation verifies arguments
+        if not all(self.is_vnode(vnode) for vnode in vnodes):
+            raise ValueError('Invalid vnode in branch')
 
-        if len(commits) != len(vnodess):
-            raise ValueError("Invalid number of arguments, should be even")
+        if not all(vnode.version.is_commit for vnode in vnodes):
+            raise ValueError('Vnode is not a commit')
 
-        inputs = list(zip(commits, map(list, vnodess)))
-        assert all(
-            not self.is_head(cmt)
-            and all(self.get_version(vn) == cmt for vn in vns)
-            for cmt, vns in inputs)
-        return inputs
+
+class BaseVersion(metaclass=ABCMeta):
+    """ Abstract base class for versions of backends """
+
+    __slots__ = ()
+
+    @property
+    @abstractmethod
+    def backend(self):
+        """ Return the backend of this vnode """
 
     @abstractmethod
-    def get_version(self, vnode):
-        """ Get the version of a vnode
+    def new_node(self):
+        """ Create a new vnode in this version
 
-        :param vnode: Vnode to query
-        :return: Version of the vnode
+        Version must be a head
+
+        :return: A new vnode in the version
         """
+        if not self.is_head:
+            raise ValueError("Can only create in head versions")
 
+    @property
     @abstractmethod
-    def is_head(self, version):
+    def is_head(self):
         """ Returns whether a version is a head or a commit
 
-        :param version: Version to query
         :return: Boolean of True if it's a head and otherwise False
         """
+
+    @property
+    def is_commit(self):
+        """ Returns whether a version is a commit or a head
+
+        :return: Boolean of True if it's a commit and otherwise False
+        """
+        return not self.is_head
+
+
+class BaseVnode(metaclass=ABCMeta):
+    """ Abstract base class for vnodes of backends """
+    __slots__ = ()
+
+    @property
+    def backend(self):
+        """ Return the backend of this vnode """
+        return self.version.backend
+
+    @property
+    @abstractmethod
+    def version(self):
+        """ Return the version of this vnode """
+
+    @abstractmethod
+    def get(self, field):
+        """ Get a field of a vnode
+
+        :param field: Field name
+        :return: Field value
+        :raises KeyError: Field not found in vnode
+        """
+
+    @abstractmethod
+    def set(self, field, value):
+        """ Set a field of a vnode
+
+        :param field: Field name
+        :param value: New value to set
+            Must be in the same version if it's also a vnode.
+        :return: None
+        :raises ValueError: Value is a vnode but isn't at the same version
+        """
+        if not self.version.is_head:
+            raise ValueError("Can only set in head versions")
+        if self.backend.is_vnode(value):
+            if self.version != value.version:
+                raise ValueError("Mismatched versions")
+
+    @abstractmethod
+    def delete(self, field):
+        """ Delete a field of a vnode
+
+        :param field: Field name
+        :return: None
+        :raises KeyError: Field not found in vnode
+        """
+        if not self.version.is_head:
+            raise ValueError("Can only delete from head versions")
+
+    def commit(self):
+        """ Commit this vnode and return the new vnode
+
+        Equivalent to extracting the vnode from self.backend.commit:
+            return self.backend.commit([self])[1][0]
+        """
+        new_version, [new_vnode] = self.backend.commit([self])
+        return new_vnode
+
+    def branch(self):
+        """ Branch from this vnode and return the new vnode
+
+        Equivalent to extracting the vnode from self.backend.branch:
+            return self.backend.branch([self])[1][0]
+        """
+        new_version, [new_vnode] = self.backend.branch([self])
+        return new_vnode
