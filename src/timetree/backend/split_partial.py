734ef04227a2b7b4e4babe9b28b0fec1cad25617
Yevhenii Diomidov <diomidov@mit.edu>
2017-12-01 00:03:11 -0500

    Fix a bug with split chain reactions

--- src/timetree/backend/split_partial.py
+++ src/timetree/backend/split_partial.py
@@ -14,7 +14,7 @@ class SplitPartialDnode(BsearchPartialDnode):
     def __init__(self):
         super().__init__()
         # TODO: Init the backend
-        self._field_backrefs = weakref.WeakKeyDictionary()
+        self._field_backrefs = weakref.WeakKeyDictionary() # This should be a weak key default dict
         self._vnode_backrefs = weakref.WeakSet()
 
     def set(self, field, value, version_num):
@@ -33,22 +33,35 @@ class SplitPartialDnode(BsearchPartialDnode):
 
         # split if necessary
         if len(self.mods_dict[field]) > 64: #TODO: better split condition
-            print('split', self)
+            # print('split', self, field, self.mods_dict['val'][-1])
             new_dnode = SplitPartialDnode()
 
+            # The order of these 4 loops is extremely important. I think I got it right this time, but I'm not 100% sure.
+
+            # copy fields
             for field, mods in self.mods_dict.items():
                 new_dnode.mods_dict[field] = [mods[-1]]
 
-            for dnode, fields in self._field_backrefs.items():
-                for field in set(fields):
-                    dnode.set(field, new_dnode, version_num)
-
+            # update head vnodes
             for vnode in set(self._vnode_backrefs):
                 if vnode.version.is_head:
                     assert vnode.version.version_num == version_num
                     self._vnode_backrefs.remove(vnode)
                     vnode.dnode = new_dnode
                     new_dnode._vnode_backrefs.add(vnode)
+
+            # update backreferences to this node
+            for field, mods in self.mods_dict.items():
+                value = mods[-1].value
+                if isinstance(value, SplitPartialDnode):
+                    value._field_backrefs[self].remove(field)
+                    value._field_backrefs[new_dnode] = value._field_backrefs.get(new_dnode, set())
+                    value._field_backrefs[new_dnode].add(field)
+
+            # update forward references to this node, possibly causing chain reactions
+            for dnode, fields in dict(self._field_backrefs).items():
+                for field in set(fields):
+                    dnode.set(field, new_dnode, version_num)
 
 
 class SplitPartialVnode(BsearchPartialVnode):
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -52,6 +52,50 @@ def test_partial_backend_many_commits(backend):
         assert commits[i][1].get('val') == i
 
 @pytest.mark.persistence_partial
+def test_partial_backend_chain_split(backend):
+    head = backend.branch()
+    vnode_a = head.new_node()
+    vnode_b = head.new_node()
+    vnode_c = head.new_node()
+    vnode_d = head.new_node()
+
+    commits = []
+    for i in range(1000):
+        vnode_a.set('val', ('a', i))
+        vnode_b.set('val', ('b', i))
+        vnode_c.set('val', ('c', i))
+        vnode_d.set('val', ('d', i))
+        vnode_a.set('a', vnode_a)
+        vnode_a.set('b', vnode_b)
+        vnode_a.set('c', vnode_c)
+        vnode_a.set('d', vnode_d)
+        vnode_b.set('a', vnode_a)
+        vnode_b.set('b', vnode_b)
+        vnode_b.set('c', vnode_c)
+        vnode_b.set('d', vnode_d)
+        vnode_c.set('a', vnode_a)
+        vnode_c.set('b', vnode_b)
+        vnode_c.set('c', vnode_c)
+        vnode_c.set('d', vnode_d)
+        vnode_d.set('a', vnode_a)
+        vnode_d.set('b', vnode_b)
+        vnode_d.set('c', vnode_c)
+        vnode_d.set('d', vnode_d)
+        commits.append(backend.commit([vnode_a, vnode_b, vnode_c, vnode_d])[1])
+
+    for i in range(1000):
+        vnode_a, vnode_b, vnode_c, vnode_d = commits[i]
+        assert vnode_a.get('val') == ('a', i)
+        assert vnode_b.get('val') == ('b', i)
+        assert vnode_c.get('val') == ('c', i)
+        assert vnode_d.get('val') == ('d', i)
+        for vnode in vnode_a, vnode_b, vnode_c, vnode_d:
+            assert vnode.get('a') == vnode_a
+            assert vnode.get('b') == vnode_b
+            assert vnode.get('c') == vnode_c
+            assert vnode.get('d') == vnode_d
+
+@pytest.mark.persistence_partial
 def test_partial_backend_keyerror(backend):
     head = backend.branch()
     vnode = head.new_node()
